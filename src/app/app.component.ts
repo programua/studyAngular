fa8cc2cbcd8d55e005d842bf112a967af36bf068
programua <thestarsandletmeplayamong@gmail.com>
2018-10-04 01:38:14 +0900

    ・onSelectメソッドの追加 ・selectedHeroプロパティの追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,10 +17,14 @@ const HEROES: Hero[] = [
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Tour of Heroes';
   heroes = HEROES;
+  selectedHero: Hero;
 
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+  }
 }
